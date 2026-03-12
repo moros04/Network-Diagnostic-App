65e43bca6022b340cacf3e0d54f4a6a6ef03ec53
moros04 <166436342+moros04@users.noreply.github.com>
2026-03-12 10:54:08 -0700

    Initial Commit

---
+++ CST380-Project/CST380_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CST380_ProjectApp.swift
+//  CST380-Project
+//
+//  Created by Miguel O on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct CST380_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
